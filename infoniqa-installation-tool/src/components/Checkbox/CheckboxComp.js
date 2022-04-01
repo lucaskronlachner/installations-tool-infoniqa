55372d60d9e0fb4ece40123e22ee80a583467787
Lucas Kronlachner <77855627+lucaskronlachner@users.noreply.github.com>
2022-04-01 10:51:21 +0200

    Little Changes

--- infoniqa-installation-tool/src/components/Checkbox/CheckboxComp.js
+++ infoniqa-installation-tool/src/components/Checkbox/CheckboxComp.js
@@ -1,11 +1,25 @@
+import React from 'react';
 import './CheckboxStyle.css';
 
 const checkboxComp = () => {
+
     return(
         <div className='container'>
             <a className='header_A'> Hello My Man</a>
         </div>
     )
+}
+
+class CheckButton extends React.Component{
+    
+    render(){
+        return (
+            <div>
+                <button>
+                </button>
+            </div>
+        )
+    }
 }
 
 export default checkboxComp;
