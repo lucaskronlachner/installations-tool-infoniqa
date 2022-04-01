e8046429c2c03ba95648a7a90afb1ee78e695409
Lucas Kronlachner <77855627+lucaskronlachner@users.noreply.github.com>
2022-04-01 08:31:28 +0200

    First Code Lines Checkbox

--- infoniqa-installation-tool/src/index.js
+++ infoniqa-installation-tool/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Checkbox from './components/Checkbox/CheckboxComp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Checkbox/>
   </React.StrictMode>,
   document.getElementById('root')
 );
