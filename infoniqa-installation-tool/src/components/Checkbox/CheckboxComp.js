dd7132518cb86ea022346e0eb80ca8899d002c22
Lucas Kronlachner <77855627+lucaskronlachner@users.noreply.github.com>
2022-04-05 12:58:59 +0200

    First_Checkbox:

--- infoniqa-installation-tool/src/components/Checkbox/CheckboxComp.js
+++ infoniqa-installation-tool/src/components/Checkbox/CheckboxComp.js
@@ -5,18 +5,31 @@ const checkboxComp = () => {
 
     return(
         <div className='container'>
-            <a className='header_A'> Hello My Man</a>
-            <CheckButton></CheckButton>
+            <CheckButton name="MyFriend"></CheckButton>
         </div>
     )
 }
 
 class CheckButton extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            name: props.name,
+            check: false
+        };
+    }
+
+    handleClick = () => {
+        this.setState({check: (this.state.check !== true)})
+    }
     
     render(){
         return (
-            <div>
-                <button className='checkBoxButton' onClick={() => {}}/>
+            <div className='container'>
+                <input type="checkbox" defaultChecked={this.state.check}/>
+                <span className='boxClass' onClick={this.handleClick}/>
+                <h1 className='DescriptionText'>{this.state.name}</h1>
             </div>
         )
     }
