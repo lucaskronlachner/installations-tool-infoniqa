79c14ceeb7e318e28a2b86a24f224f5d442fdb89
elpanic <leonhard.panic@outlook.de>
2022-04-06 12:26:13 +0200

    Fixed index.js and packages.json

--- infoniqa-installation-tool/src/index.js
+++ infoniqa-installation-tool/src/index.js
@@ -7,6 +7,7 @@ import Button from './components/Button/ButtonComp';
 import TextInputField from './components/Text_Input/TextInputComp';
 import SwitchComp from './components/SwitchComp/SwitchComp';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Checkbox/>
@@ -14,16 +15,12 @@ ReactDOM.render(
     <Button
     type="button"
     buttonStyle="btn--default--solid"
-<<<<<<< HEAD
     buttonSize="btn--medium">Press Me :)</Button>
 
     <TextInputField></TextInputField>
-=======
-    buttonSize="btn--medium">Button Text</Button>
-    <TextInputField size="txtInput_large"></TextInputField>
     <SwitchComp isOn={true}/>
->>>>>>> 0a05c7295bc60190ec9e997bf9516ad1a7443209
   </React.StrictMode>,
+
   document.getElementById('root')
 );
 
