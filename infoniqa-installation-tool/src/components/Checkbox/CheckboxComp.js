aea3236942b83f252da4292ec69286bbbcd5f092
Lucas Kronlachner <77855627+lucaskronlachner@users.noreply.github.com>
2022-05-25 11:06:37 +0200

    Finished Converter

--- infoniqa-installation-tool/src/components/Checkbox/CheckboxComp.js
+++ infoniqa-installation-tool/src/components/Checkbox/CheckboxComp.js
@@ -29,7 +29,7 @@ class CheckButton extends React.Component {
                 <div className="checkbox-component">
                     <span className="checkbox-check-element">
                         <div className="chech-svg-container">
-                            <svg width="12px" height="10px" viewbox="0 0 12 10">
+                            <svg width="12px" height="10px" viewBox="0 0 12 10">
                                 <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
                             </svg>
                         </div>
