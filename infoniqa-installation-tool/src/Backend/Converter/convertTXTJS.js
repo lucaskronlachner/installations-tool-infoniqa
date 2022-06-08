eacc7d2d4b2457c15cfa2939bca809d7ceac30c4
Lucas Kronlachner <77855627+lucaskronlachner@users.noreply.github.com>
2022-06-08 10:43:04 +0200

    Converting JS-Files

---
+++ infoniqa-installation-tool/src/Backend/Converter/convertTXTJS.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import { Checkbox, SectionComp, SectionItemComp, FileUploadComp, DropDownComp, ColorPickerComp, TextInputField, SwitchComp, DatePickerComp, RadioComp, Button} from '../../components/modules';
+
+function PageContainer(props) {
+    const folder_src = props.fldr_src
+    const [template_src, setTemplate_src] = useState("")
+
+    useEffect(() => {
+        
+    })
+
+
+}
